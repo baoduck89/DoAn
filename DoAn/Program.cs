e26511e2d196e70de42acfe416839fbff8a63626
Đức Bảo <ndbpro99@gmail.com>
2020-10-15 20:39:32 +0700

    .

---
+++ DoAn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoAn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ DoAn/Utility/dangnhap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoAn.NewFolder4
+{
+    public class dangnhap
+    {
+
+        private string tk, mk;
+        public void Hien()
+        {
+            int dem = 0;
+            while (dem < 3)
+            {
+                dem++;
+                Console.WriteLine("-------------Dang Nhap-------------");
+                Console.Write("Tai Khoan:");
+                tk = Console.ReadLine();
+                Console.WriteLine("Mat Khau:");
+                mk = Console.ReadLine();
+                if (tk == "admin" && mk = "admin")
+                {
+                    Console.WriteLine(" DANG NHAP THANH CONG!");
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
